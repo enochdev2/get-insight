14505eecfbbb3718a3a752f898f11695bcdc6bfe
enoch-promise <enochpromise.va@gmail.com>
2023-12-13 20:19:59 -0800

    new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Headers from "./Headers";
+import { authOptions } from "@/lib/authOptions";
 
 
 const Navbar = () => {
