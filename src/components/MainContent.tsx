96241970944a46378f49e9b9068b6007c6cf784e
enoch-promise <enochpromise.va@gmail.com>
2024-01-04 19:19:31 -0800

    commit

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -23,7 +23,7 @@ const MainContent = () => {
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">Dev-Noch</h1>
+          <h1 className="Welcome-text mt-5 text-[13px]">Dev-Noch</h1>
         </motion.div>
 
         <motion.div
