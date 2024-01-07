574d4a66ac704c4a7489829885f35b46b736e272
enoch-promise <enochpromise.va@gmail.com>
2024-01-06 20:43:29 -0800

    edit3

--- src/components/ServicesOffer.tsx
+++ src/components/ServicesOffer.tsx
@@ -15,7 +15,7 @@ const ServicesOffer = () => {
           </h2>
           <div>
             <h4 className="underline font-semibold text-xl">Stack</h4>
-            <p className="flex gap-3" ap-3">
+            <p className="flex gap-3">
             <IoCheckmarkCircleSharp />
               Tailwind
             </p>
