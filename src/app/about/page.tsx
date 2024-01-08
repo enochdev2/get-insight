b81f9e30aab3874a2f68bb7cedda8ef678d3c3b8
enoch-promise <enochpromise.va@gmail.com>
2024-01-08 04:56:59 -0800

    deploy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,24 +14,10 @@ const About = () => {
       <div className="w-5/6 m-auto text-xl py-8 px-5 rounded-lg bg-slate-200 dark:bg-transparent">
         <h2 className="text-teal-900 underline font-bold my-3 text-2xl">About Us </h2>
         <p className="text-justify my-4 text-[18px]">
-          deserunt nihil tenetur, magnam distinctio vero officia minus
-          dignissimos voluptates, fugit velit. Numquam blanditiis animi tenetur
-          autem hic! Ipsum quam accusamus maxime quisquam in deserunt mollitia
-          fuga autem architecto corrupti alias officia laboriosam doloribus,
-          dolor neque nemo voluptatum illum aperiam eaque consequuntur vel?
-          Asperiores ut quasi eveniet numquam at blanditiis necessitatibus
-          dolorum, neque illum hic magnam modi deserunt sapiente fuga, nesciunt
-          quam.
+         Dev-Noch Technology is a Web Development company having the goal of providing our clients with  the  web  user experiences. Delivering top-quality software solutions that tackle complex business challenges.
         </p>
         <p className="text-justify my-4 text-[18px]">
-          Architecto illum delectus libero repellat. Consequatur minus quidem
-          alias doloribus fugit, fugiat fuga vero illum amet. Consequatur
-          dolorum laudantium voluptas distinctio alias voluptatum corporis
-          inventore unde, magnam molestias? Sed architecto omnis obcaecati ut
-          exercitationem modi error doloribus possimus dolores! Veritatis
-          expedita incidunt qui, numquam ipsam omnis sunt, doloribus fugit est
-          repellat a reprehenderit soluta mollitia molestias dignissimos cumque
-          accusantium!
+         
         </p>
       </div>
     </section>
