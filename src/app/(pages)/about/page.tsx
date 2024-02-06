d92276357a24f0bcded0a75b28f2527b4c893369
enoch-promise <enochpromise.va@gmail.com>
2024-02-06 01:30:39 -0800

    Commit

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,66 +1,87 @@
 import { Metadata } from "next";
 import React from "react";
 
-
 export const metadata: Metadata = {
   title: "Dev-Noch",
   description: "about Dev-Noch",
 };
 
-
 const About = () => {
   return (
     <section className="w-full min-h-screen py-12 ">
       <div className="w-5/6 m-auto text-xl py-8 px-5 rounded-lg bg-slate-200 dark:bg-transparent">
-        <h2 className="text-teal-900 underline font-bold my-3 text-2xl">
+        <h2 className="text-center text-teal-900 underline font-bold mx-auto my-3 text-2xl">
           About Us{" "}
         </h2>
-        <p className="text-justify my-4 text-[18px]">
+        <p className="text-justify my-4 text-[16px]">
           Welcome to Dev-noch Technology, where innovation meets expertise in
           the world of Full-Stack development. We are a dynamic and
           client-focused team committed to crafting digital solutions that
           propel businesses to new heights. With a passion for technology and a
           dedication to excellence, we stand as your reliable partner in the
-          fast-evolving landscape of web and app development. **Our Mission** At
-          Dev-noch Technology, our mission is to transform ideas into impactful
-          digital experiences. We strive to empower businesses by leveraging
-          cutting-edge technologies and a comprehensive Full-Stack approach.
-          Whether you're a startup navigating uncharted waters or an established
-          enterprise seeking to redefine your online presence, we're here to
-          architect solutions tailored to your unique needs. **What Sets Us
-          Apart** * **Full-Stack Expertise:** Our team of seasoned developers
-          possesses expertise across the entire technology stack. From front-end
-          design to back-end functionality, we bring a holistic approach to
-          every project, ensuring seamless integration and optimal performance.
-          * **Innovation at Core:** In the ever-evolving tech landscape, staying
-          ahead requires a commitment to innovation. At Dev-noch Technology, we
-          embrace the latest tools, frameworks, and methodologies, ensuring that
-          your projects are not just current but future-proof. *
-          **Client-Centric Approach:** Your success is our priority. We believe
-          in building strong, collaborative relationships with our clients. From
-          the initial consultation to project delivery and beyond, we are
-          committed to open communication, transparency, and delivering
-          solutions that exceed expectations. * **Agile Development:** We
-          understand the importance of adaptability in the digital realm. Our
-          agile development methodology ensures flexibility, allowing us to
-          respond promptly to changes, deliver faster, and maintain a high level
-          of quality throughout the development process. **Our Services** *
-          **Web Development:** Crafting visually stunning and functionally
-          robust websites that make a lasting impression. * **App Development:**
-          From concept to deployment, we specialize in creating intuitive and
-          powerful applications for various platforms. * **E-Commerce
-          Solutions:** Driving online success with tailored e-commerce solutions
-          that enhance user experience and boost sales. * **Custom Software:**
-          Designing and developing bespoke software solutions to address
-          specific business challenges and opportunities. **Let's Build
-          Together** Embark on a journey of digital transformation with Dev-noch
-          Technology. We believe in the power of collaboration and the potential
-          of technology to drive success. Join hands with us, and let's build
-          solutions that not only meet today's requirements but also pave the
-          way for a future of innovation and growth. Your vision, our
-          expertise—let's make it happen.
+          fast-evolving landscape of web and app development.
         </p>
-        <p className="text-justify my-4 text-[18px]"></p>
+        <div>
+          <p className="text-center text-teal-900 underline font-bold my-3 text-xl">
+            Our Mission
+          </p>
+          <p className="text-justify my-4 text-[16px]">
+            At Dev-noch Technology, our mission is to transform ideas into
+            impactful digital experiences. We strive to empower businesses by
+            leveraging cutting-edge technologies and a comprehensive Full-Stack
+            approach. Whether you're a startup navigating uncharted waters or an
+            established enterprise seeking to redefine your online presence,
+            we're here to architect solutions tailored to your unique needs.
+            <p className="text-teal-900 text-center underline font-bold my-3 text-xl">
+              What Sets Us Apart
+            </p>
+            Full-Stack Expertise: Our team of seasoned developers possesses
+            expertise across the entire technology stack. From front-end design
+            to back-end functionality, we bring a holistic approach to every
+            project, ensuring seamless integration and optimal performance.
+            <p className="text-teal-900 underline font-bold my-3 text-xl">
+              Innovation at Core:
+            </p>
+            In the ever-evolving tech landscape, staying ahead requires a
+            commitment to innovation. At Dev-noch Technology, we embrace the
+            latest tools, frameworks, and methodologies, ensuring that your
+            projects are not just current but future-proof.
+            <p className="text-center text-teal-900 underline font-bold my-3 text-xl">
+              Client-Centric Approach:
+            </p>
+            Your success is our priority. We believe in building strong,
+            collaborative relationships with our clients. From the initial
+            consultation to project delivery and beyond, we are committed to
+            open communication, transparency, and delivering solutions that
+            exceed expectations.
+            <p>Agile Development:</p>
+            We understand the importance of adaptability in the digital realm.
+            Our agile development methodology ensures flexibility, allowing us
+            to respond promptly to changes, deliver faster, and maintain a high
+            level of quality throughout the development process.
+            <p className="text-teal-900 underline font-bold my-3 text-xl text-center">
+              Our Services
+            </p>
+            <span className="underline">Web Devlelopment:</span> {""}
+            Crafting visually stunning and functionally robust websites that
+            make a lasting impression. <br />
+            App Development: From concept to deployment, we specialize in
+            creating intuitive and powerful applications for various platforms.
+            <br />
+            <span className="underline">E-Commerce Solutions:</span> {""}
+            Driving online success with tailored e-commerce solutions that
+            enhance user experience and boost sales. <br />
+            <span className="underline">Custom Software:</span> {""}
+            Designing and developing bespoke software solutions to address
+            specific business challenges and opportunities. Let's Build Together
+            Embark on a journey of digital transformation with Dev-noch
+            Technology. We believe in the power of collaboration and the
+            potential of technology to drive success. Join hands with us, and
+            let's build solutions that not only meet today's requirements but
+            also pave the way for a future of innovation and growth. Your
+            vision, our expertise—let's make it happen.
+          </p>
+        </div>
       </div>
     </section>
   );
