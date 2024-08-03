db64bc78144df48dcad6e8c7ffae02db41ce58d9
enochdev2 <enochthechristepistle@gmail.com>
2024-08-03 07:40:41 +0100

    front

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Image from "next/image";
 import React from "react";
 
 export const metadata: Metadata = {
@@ -10,10 +11,24 @@ const About = () => {
   return (
     <section className="w-full min-h-screen py-12 ">
       <div className="lg:mx-20 mt-12 mx-14 text-justify">
-        <h1 className="underline font-bold text-xl dark:text-white text-center">
+      <div className=" relative max-w-[100%] mb-10 lg:h-96 h-[430px]">
+          <Image src="/RealInisight.jpg" alt="Real-Insight" fill />
+        </div>
+        <div className="z-10 md:w-2/3 w-full bg-slate-300 absolute translate-x-[-50%] md:translate-x-[-50%] translate-y-[-20%] lg:translate-y-[20%] left-[50%] bg-transparent top-48 font-semibold m-auto text-center py-8 px-2 dark:text-black">
+          <h1 className="font-bold text-2xl lg:text-3xl my-2 bg-gradient-to-r from-cyan-600 via-green-600 to-teal-500 text-bold text-transparent bg-clip-text">
+            Welcome to Dev-Noch Blog,
+          </h1>
+          <h4 className="md:text-2xl bg-black/40 rounded-lg py-3 px-4 text-white text-lg font-sans w-9/12 m-auto ">
+            Your ultimate destination for insightful and enriching content.
+            Here, we embark on a journey through the realms of business,
+            finance, family, and technology, offering you a treasure trove of
+            knowledge and wisdom.
+          </h4>
+        </div>
+        <h1 className="underline font-bold text-xl mb-4 dark:text-white text-center">
           About Dev-Noch Blog
         </h1>
-        <p>
+        <p className="text-lg font-medium my-4 max-w-6xl mx-auto">
           Welcome to Dev-Noch Blog, where we explore the intersections of
           business, finance, family, and technology to provide insightful
           articles and resources for our readers. Here, we embark on a journey
@@ -23,18 +38,20 @@ const About = () => {
         <h1 className="underline font-bold text-xl dark:text-white text-center">
           Our Mission At Dev-Noch Blog
         </h1>
-        <p>
+        <p className="text-lg font-medium my-4 max-w-6xl mx-auto">
           our mission is to empower individuals and businesses alike with
           knowledge and expertise in the realms of business strategies,
           financial management, familial dynamics, and technological
           advancements. We strive to deliver high-quality content that educates,
           inspires, and engages our audience.
         </p>
+
+        
         <h1 className="underline font-bold text-xl dark:text-white text-center">
           What We Cover
         </h1>
-        <ul>
-          <li>
+        <ul className="text-lg font-medium my-4 max-w-6xl mx-auto space-y-2">
+          <li >
             Business : Explore cutting-edge business trends, entrepreneurial
             insights, and strategies for success in today&lsquo;s competitive
             landscape.
@@ -55,16 +72,21 @@ const About = () => {
             individuals.
           </li>
         </ul>
+        
+        <div className=" relative max-w-[60%] mx-auto mb-10 lg:h-96 h-[250px]">
+            <Image src="/insightpeoples.jpg" alt="Real-Insight" fill />
+          </div>
         {/* <h1 className="underline font-bold text-xl dark:text-white text-center">
           Meet the Team
-        </h1>
-        [Include a brief introduction about yourself and any contributors or
-        team members involved in creating content for the blog. Highlight
-        relevant expertise, experiences, and passions related to the topics
-        covered on the blog.] */}
+          </h1>
+          [Include a brief introduction about yourself and any contributors or
+          team members involved in creating content for the blog. Highlight
+          relevant expertise, experiences, and passions related to the topics
+          covered on the blog.] */}
         <h1 className="underline font-bold text-xl dark:text-white text-center">
           Our Values
         </h1>
+        <p className="text-lg font-medium my-4 max-w-6xl mx-auto">
         <br />
         Quality: We are committed to delivering well-researched, accurate, and
         insightful content to our readers.
@@ -82,7 +104,8 @@ const About = () => {
         your content by leaving comments, sharing articles with their networks,
         and reaching out with feedback or topic suggestions.]
         <br />
-        <p>
+        </p>
+        <p className="text-lg font-medium my-4 max-w-6xl mx-auto">
           Thank you for visiting Dev-Noch Blog. We hope you find our content
           valuable and look forward to journeying with you as we explore the
           fascinating worlds of business, finance, family, and technology
