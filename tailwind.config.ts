f38d4fd8be9b3a52c94ef562dc3d3620e8b6d784
enochdev2 <enochthechristepistle@gmail.com>
2024-08-07 07:52:15 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'custom-image': "url('/blockchain.svg')",
+        'custom-image': "url('/blockchain.png')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
